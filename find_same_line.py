0ab6080f3932b4d31b5041fbd6bf9643e9bdbb1d
AAowe@outlook.com <proleeibit@outlook.com>
2025-04-02 11:57:57 +0800

    update 2 python scripts

--- find_same_line.py
+++ find_same_line.py
@@ -3,15 +3,27 @@ import sys
 def find_common_lines(file1, file2, file3):
     # 读取每个文件的行
     with open(file1, 'r') as f1:
-        lines1 = set(f1.readlines())
+        lines1 = f1.readlines()
+        lines1_set = set(lines1)
     with open(file2, 'r') as f2:
-        lines2 = set(f2.readlines())
+        lines2 = f2.readlines()
+        lines2_set = set(lines2)
     with open(file3, 'r') as f3:
-        lines3 = set(f3.readlines())
+        lines3 = f3.readlines()
+        lines3_set = set(lines3)
     
     # 找出三个文件中的共同行
-    common_lines = lines1.intersection(lines2).intersection(lines3)
-    
+    common_lines = list(lines1_set.intersection(lines2_set).intersection(lines3_set))
+
+    # ordered as lines1
+    order = 1
+    match order:
+        case 2:
+            common_lines.sort(key = lines2.index)
+        case 3:
+            common_lines.sort(key = lines3.index)
+        case _:
+            common_lines.sort(key = lines1.index)
     # 输出结果
     print("Common lines in all three files:")
     for line in common_lines:
@@ -19,11 +31,10 @@ def find_common_lines(file1, file2, file3):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <file1> <file2> <file3>")
+        print(f"Usage: python {sys.argv[0]} <file1> <file2> <file3>")
         sys.exit(1)
     
     file1 = sys.argv[1]
     file2 = sys.argv[2]
     file3 = sys.argv[3]
-    
     find_common_lines(file1, file2, file3)
--- lines_in_a_not_in_b.py
+++ lines_in_a_not_in_b.py
@@ -3,14 +3,17 @@ import sys
 def find_lines_in_a_not_in_b(file_a, file_b):
     # 读取文件 A 的行
     with open(file_a, 'r') as f_a:
-        lines_a = set(f_a.readlines())
+        lines_a = f_a.readlines()
+        lines_a_set = set(lines_a)
     
     # 读取文件 B 的行
     with open(file_b, 'r') as f_b:
-        lines_b = set(f_b.readlines())
+        lines_b = f_b.readlines()
+        lines_b_set = set(lines_b)
     
     # 找出在文件 A 中但不在文件 B 中的行
-    lines_in_a_not_in_b = lines_a - lines_b
+    lines_in_a_not_in_b = list(lines_a_set - lines_b_set)
+    lines_in_a_not_in_b.sort(key = lines_a.index)
     
     # 输出结果
     print("Lines in {} but not in {}:".format(file_a, file_b))
@@ -19,7 +22,7 @@ def find_lines_in_a_not_in_b(file_a, file_b):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python script.py <file_a> <file_b>")
+        print(f"Usage: python {sys.argv[0]} <file_a> <file_b>")
         sys.exit(1)
     
     file_a = sys.argv[1]
